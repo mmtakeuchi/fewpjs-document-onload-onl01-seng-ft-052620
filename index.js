a614c7cf8268ceed213d080dbc597f70c63d9910
mmtakeuchi <mikemtaki@gmail.com>
2020-08-26 10:06:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+let change = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", function () {
+    change.textContent = "This is really cool!";
+});
